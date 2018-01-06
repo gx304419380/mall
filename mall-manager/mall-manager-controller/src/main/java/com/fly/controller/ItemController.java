9d5a4a7656a8d835e631c298fe231c84f044ae89
tiny <30303760+gx304419380@users.noreply.github.com>
2018-01-06 10:39:46 +0800

    controller测试，tomcat运行成功

--- mall-manager/mall-manager-controller/src/main/java/com/fly/controller/ItemController.java
+++ mall-manager/mall-manager-controller/src/main/java/com/fly/controller/ItemController.java
@@ -1,10 +1,29 @@
 package com.fly.controller;
 
+import com.fly.pojo.Item;
+import com.fly.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("item/")
 public class ItemController {
+    @Autowired
+    private ItemService itemService;
 
-
+    /**
+     * 根据id查询商品
+     * @param id
+     * @return
+     */
+    @RequestMapping("{id}")
+    @ResponseBody
+    public Item findItemById(@PathVariable Long id) {
+        Item item = itemService.selectByPrimaryKey(id);
+        return item;
+    }
 
 }
