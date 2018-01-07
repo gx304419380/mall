2d074eb7245b1e8fbb213c59b36e695f74df7e26
tiny <30303760+gx304419380@users.noreply.github.com>
2018-01-07 11:21:07 +0800

    重构项目：pojo加入序列化接口

--- mall-manager/mall-manager-pojo/src/main/java/com/fly/pojo/Item.java
+++ mall-manager/mall-manager-pojo/src/main/java/com/fly/pojo/Item.java
@@ -1,8 +1,9 @@
 package com.fly.pojo;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class Item {
+public class Item implements Serializable {
     private Long id;
 
     private String title;
--- mall-manager/mall-manager-pojo/src/main/java/com/fly/pojo/OrderItem.java
+++ mall-manager/mall-manager-pojo/src/main/java/com/fly/pojo/OrderItem.java
@@ -1,6 +1,8 @@
 package com.fly.pojo;
 
-public class OrderItem {
+import java.io.Serializable;
+
+public class OrderItem implements Serializable {
     private String id;
 
     private String itemId;
